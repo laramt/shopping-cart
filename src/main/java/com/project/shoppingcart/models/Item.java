3327ce599b9fff313aff22eae5d67bbc3d1974bb
Lara Menck <lara.menckt@gmail.com>
2022-11-10 15:10:50 -0300

    feat: Item model and repository

---
+++ src/main/java/com/project/shoppingcart/models/Item.java
@@ -0,0 +1,68 @@
+package com.project.shoppingcart.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Item {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	@OneToOne
+	private Product product;
+	private int quantity;
+	/* @OneToOne
+	 * @JsonIgnore
+	private Cart cart; */
+	
+	public Item() {
+		super();
+	}
+
+	public Item(Long id, Product product, int quantity) {
+		super();
+		this.id = id;
+		this.product = product;
+		this.quantity = quantity;
+		//this.cart = cart;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	/* public Cart getCart() {
+		return cart;
+	}
+
+	public void setCart(Cart cart) {
+		this.cart = cart;
+	}
+	*/
+	
+	
+}
